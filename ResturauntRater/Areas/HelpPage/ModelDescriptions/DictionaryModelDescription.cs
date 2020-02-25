2d725c43a2ae57336a560894f987b8cfce0d6ce5
Jerome Flaherty <jayromulus.f@gmail.com>
2020-02-25 10:44:12 -0500

    Add project files.

---
+++ ResturauntRater/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ResturauntRater.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ResturauntRater/Models/Restaurant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+// square brackets are giving each property information such as the [Key] being the primatry key, [Required] means it is required to add it to the database, and [Range(min,max)] giving it a range of numbers it is allowed to be
+namespace ResturauntRater.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Style { get; set; }
+
+        [Required]
+        [Range(0f,5f)]
+        public float Rating { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public int DollarSigns { get; set; }
+    }
+}
